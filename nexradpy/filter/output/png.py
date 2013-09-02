f8c8252cb04a44e731496054f0e3761080f527c3
Scott Drake <demonburrito@gmail.com>
2013-09-02 17:39:43 -0500

    Issue #1: Added some drawing logic.

--- nexradpy/filter/output/png.py
+++ nexradpy/filter/output/png.py
@@ -1,3 +1,4 @@
+import math
 from . import OutputFilter, NexradpyError
 import numpy
 try:
@@ -11,6 +12,14 @@ class PNG(OutputFilter):
     radial_matrix = None
 
     output_dimensions = None
+    bb = None
+
+    # ImageDraw instance
+    draw = None
+
+    def __init__(self):
+        self.im = Image.open()
+        self.draw = ImageDraw(self.im)
 
     def _create_radial_matrix(self):
         """From a Product, create a Numpy matrix from radials."""
@@ -39,5 +48,27 @@ class PNG(OutputFilter):
         self.output_dimensions = (d, d)
         self.center = (r, r)
 
-    def _draw_radial(self, start_angle, delta_angle, levels):
-        pass
+    def _levels(self, value):
+        """Return a RGB tuple based on value."""
+        return value
+
+    def _draw_radial(self, angle_tuple_deg, bins):
+        """Draws a radial. First value of angle tuple is start angle, second
+        value is angle delta."""
+        start_angle = 0
+        end_angle = 0
+
+        start_angle = math.radians(angle_tuple_deg[0])
+        end_angle = math.radians(angle_tuple_deg[0] + angle_tuple_deg[1])
+
+        last_coord = self.center
+        for bn in bins:
+            # first bin
+            if n == 0:
+                points = [self.center]
+                points[1] = self.center[0] + (n + 1) * Math.cos(start_angle),
+                    self.center[1] + (n + 1) * Math.sin(start_angle)
+                points[2] = self.center[0] + (n + 1) * Math.cos(end_angle),
+                    self.center[1] + (n + 1) * Math.sin(end_angle)
+
+                self.draw.polygon(points, fill=_levels(bn))
