274a3cc7387edd29bc81052bf96cc1f5292f4ca6
Scott Drake <demonburrito@gmail.com>
2013-09-03 22:08:50 -0500

    Issue #1: PNG Filter
    Basic png support.

--- nexradpy/filter/output/png.py
+++ nexradpy/filter/output/png.py
@@ -1,6 +1,8 @@
 import math
 from . import OutputFilter, NexradpyError
 import numpy
+import math
+
 try:
     from PIL import Image, ImageDraw
 except ImportError:
@@ -9,38 +11,17 @@ except ImportError:
 class PNG(OutputFilter):
     """Outputs a PNG from a decoded product."""
     p = None
-    radial_matrix = None
-
     output_dimensions = None
-    bb = None
 
     # ImageDraw instance
     draw = None
 
-    def __init__(self):
-        self.im = Image.open()
-        self.draw = ImageDraw(self.im)
-
-    def _create_radial_matrix(self):
-        """From a Product, create a Numpy matrix from radials."""
-        pass
-
-    def _radial_to_bitmap(self):
-        pass
+    # Image instance
+    im = None
 
     def filter(self, p):
         """Takes a product and returns a PNG."""
         self.p = p
-
-        l = []
-        try:
-            for radial in p['symbology']['layers'][0]['data']['radials']:
-                l.append(radial['levels'])
-
-            self.radial_matrix = numpy.matrix(l)
-
-        except Exception as ex:
-            raise NexradpyError(ex)
 
         # for simplicity, make the diameter no_of_bins * 2
         r = p['symbology']['layers'][0]['data']['number_bins']
@@ -48,27 +29,67 @@ class PNG(OutputFilter):
         self.output_dimensions = (d, d)
         self.center = (r, r)
 
+        self.im = Image.new('RGBA', self.output_dimensions)
+        self.draw = ImageDraw.Draw(self.im)
+
+        for radial in p['symbology']['layers'][0]['data']['radials']:
+            self._draw_radial((radial['start_angle'] / 10, radial['delta_angle'] / 10),
+                    radial['levels'])
+
+        return self.im
+
     def _levels(self, value):
         """Return a RGB tuple based on value."""
-        return value
+        if value == 0:
+            return (0,0,0,0)
+        else:
+            return (value, value, value)
 
     def _draw_radial(self, angle_tuple_deg, bins):
         """Draws a radial. First value of angle tuple is start angle, second
-        value is angle delta."""
-        start_angle = 0
-        end_angle = 0
-
+        value is angle delta.
+        x = r*cos(t) + h
+        y = r*sin(t) + k
+        """
         start_angle = math.radians(angle_tuple_deg[0])
         end_angle = math.radians(angle_tuple_deg[0] + angle_tuple_deg[1])
 
-        last_coord = self.center
+        point_a = ()
+        point_b = ()
+        r = 1
         for bn in bins:
             # first bin
-            if n == 0:
+            if r == 1:
                 points = [self.center]
-                points[1] = self.center[0] + (n + 1) * Math.cos(start_angle),
-                    self.center[1] + (n + 1) * Math.sin(start_angle)
-                points[2] = self.center[0] + (n + 1) * Math.cos(end_angle),
-                    self.center[1] + (n + 1) * Math.sin(end_angle)
 
-                self.draw.polygon(points, fill=_levels(bn))
+                points.append( (self.center[0] + math.cos(start_angle),
+                    self.center[1] + math.sin(start_angle)) )
+
+                points.append( (self.center[0] + math.cos(end_angle),
+                    self.center[1] + math.sin(end_angle)) )
+
+                self.draw.polygon(points, outline=self._levels(bn), fill=self._levels(bn))
+
+                point_a = points[1]
+                point_b = points[2]
+                r += 1
+            else:
+                # we just need two more points to draw the next bin
+                points = []
+                points.append(
+                    (self.center[0] + ((r + 1) * math.cos(start_angle)),
+                    self.center[1] + ((r + 1) * math.sin(start_angle)) )
+                )
+
+                points.append(
+                    (self.center[0] + ((r + 1) * math.cos(end_angle)),
+                    self.center[1] + ((r + 1) * math.sin(end_angle)) )
+                )
+
+                points.append( point_a )
+                points.append( point_b )
+                self.draw.polygon(points, outline=self._levels(bn), fill=self._levels(bn))
+                r += 1
+
+                point_a = points[0]
+                point_b = points[1]
